068185aa381f6056ed5bbe5e37c2d7d135140c1b
caick Tanan <caick_tanan@hotmail.com>
2020-06-23 16:51:54 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println();
+		
+		System.out.println("=== Test 5: Seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setEmail("MarthaWaine@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 
 	}
 
